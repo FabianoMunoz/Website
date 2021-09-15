d262acb4dbc840815fee32aa53de43e1f8fceed1
Fabiano Munoz <90644404+FabianoMunoz@users.noreply.github.com>
2021-09-15 16:22:10 +0200

    Update psw.js

--- psw.js
+++ psw.js
@@ -5,19 +5,17 @@ var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
 
-// When the user clicks on the password field, show the message box
+
 myInput.onfocus = function() {
   document.getElementById("message").style.display = "block";
 }
 
-// When the user clicks outside of the password field, hide the message box
 myInput.onblur = function() {
   document.getElementById("message").style.display = "none";
 }
 
-// When the user starts to type something inside the password field
+
 myInput.onkeyup = function() {
-  // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
   if(myInput.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
@@ -27,7 +25,7 @@ myInput.onkeyup = function() {
     letter.classList.add("invalid");
 }
 
-  // Validate capital letters
+  
   var upperCaseLetters = /[A-Z]/g;
   if(myInput.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
@@ -37,7 +35,7 @@ myInput.onkeyup = function() {
     capital.classList.add("invalid");
   }
 
-  // Validate numbers
+ 
   var numbers = /[0-9]/g;
   if(myInput.value.match(numbers)) {
     number.classList.remove("invalid");
@@ -47,7 +45,7 @@ myInput.onkeyup = function() {
     number.classList.add("invalid");
   }
 
-  // Validate length
+  
   if(myInput.value.length >= 8) {
     length.classList.remove("invalid");
     length.classList.add("valid");
